d138d0ea7ab28da8c36ed534544991239c1a350a
Sabina <balejikovasabina@gmail.com>
2024-10-01 21:59:26 +0200

    auth mobile margin update

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -13,7 +13,7 @@ export default function SigninPage() {
   };
 
   return (
-    <div className="flex flex-col items-center mt-16 lg:mt-40 h-full">
+    <div className="flex flex-col items-center mt-16 mb-16 md:mb-0 lg:mt-40 h-full">
       <h1 className="mb-8 tracking-tight">Sign in</h1>
       <TheSigninForm onSuccessfulLogin={handleSuccessfulLogin} />
       <small className="mt-8">
